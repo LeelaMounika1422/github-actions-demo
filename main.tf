d11b4b2414071507f9c0e2b32d7b4156c3834e62
LeelaMounika1422 <mounika.garapati001@gmail.com>
2026-09-09 16:50:44 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-github-actions-demo"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stgithubactionsdemo01"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
